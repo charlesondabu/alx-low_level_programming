7eea3edfade794302bd58de8ac222fed0b0121cc
Charles Ondabu <info@condabu>
2023-06-15 15:33:20 -0700

    Fixed Formarting isues

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,14 +9,15 @@
  */
 int main(void)
 {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t bytes_written = write(STDERR_FILENO, message, strlen(message));
+	char message[] = "and that piece of art is useful\" - +\
+			  Dora Korpar, 2015-10-19\n";
+	ssize_t bytes_written = write(STDERR_FILENO, message, strlen(message));
 
-    if (bytes_written == -1)
-    {
-     write(STDERR_FILENO, "Error writing to standard error\n", strlen("Error writing to standard error\n"));
-     return (1);
-    }
-
-    return (1);
+	if (bytes_written == -1)
+	{
+		write(STDERR_FILENO, "Error writing to standard error\n", +\
+				strlen("Error writing to standard error\n"));
+		return (1);
+	}
+	return (1);
 }
