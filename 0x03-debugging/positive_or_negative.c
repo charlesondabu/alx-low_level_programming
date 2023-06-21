1ffe120099068af62af770b7e85ac35b3fa3a9a2
Charles Ondabu <info@condabu>
2023-06-21 05:41:19 -0700

    initial commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.c"
+/**
+* postive_or_negative - check if + or -
+* Return: int
+*/
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d is negative", i);
+	}
+	else if (i > 0)
+	{
+		printf("%d is positive", i);
+	}
+	else
+	{
+		printf("%d is zero", i);
+	}
+}
