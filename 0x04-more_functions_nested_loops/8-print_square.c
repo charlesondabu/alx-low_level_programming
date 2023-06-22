5c8af22d9871bd9cf414686d352dc9fd467cb777
Charles Ondabu <info@condabu>
2023-06-22 09:49:06 -0700

    Fixed 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - print x size
+ * @s: sixe to print
+ * Return: void
+ */
+void print_square(int s)
+{
+	int a, b;
+
+	if (s <= 0)
+	{
+		_putchar('\n');
+	}
+	for (a = 0; a <= s; a++)
+	{
+		for (b = 0; b <= s; b++)
+		{
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
 int _isupper(int c);
 int _putchar(char s);
 int _isdigit(int c);
+void print_triangle(int size);
+void print_square(int size);
