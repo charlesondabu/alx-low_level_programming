d6309d71f4c8819f7b609500157458cccd49af72
Charles Ondabu <info@condabu>
2023-06-23 01:24:42 -0700

    fixed 100-times_tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,50 @@
 #include "main.h"
 
 /**
-  * print_times_table - Prints the n times table, starting with 0
-  * @n: number of times to print
-  * Return: void
-  */
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: number of times to print
+ * Return: void
+ */
 void print_times_table(int n)
 {
-	int a, b, c, u, d;
-	
-	if (n < 0|| n > 15)
+	int x, y, z;
+
+	if (n >= 0 && n <= 14)
 	{
-		return;
-	}
-	for (a = 0; a <= n; a++)
-	{
-		for (b = 0; b <= n; b++)
+		for (x = 0; x <= n; x++)
 		{
-			c = a * b;
-
-			if (c > n)
+			for (y = 0; y <= n; y++)
 			{
-				u = c % 10;
-				d = (c - u) / 10;
-
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
-			}
-			else
-			{
-				if (b != 0)
+				z = x * y;
+				if (z > 99)
 				{
-					_putchar(44);
+					_putchar(',');
 					_putchar(32);
-					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-
-				_putchar(c + '0');
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
 			}
+			_putchar('\n');
 		}
-
-		_putchar('\n');
 	}
 }
